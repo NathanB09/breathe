2cfa64508d6d931a1003f3e045fdc1f394e50843
Nathan Bell <nathanbell09@gmail.com>
2021-04-21 17:29:14 +0100

    added to initial state

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,15 +1,25 @@
 import React, { createContext, useReducer } from 'react'
-import Reducer from './Reducer'
+import reducer from './reducer'
 import PropTypes from 'prop-types'
 
 const initialState = {
   round: 0,
   roundCount: 3,
-  rounds: {},
+  breathCount: 30,
+  round1: 1000,
+  round2: 1000,
+  round3: 1000,
+  round4: 1000,
+  round5: 1000,
+  round6: 1000,
+  round7: 1000,
+  round8: 1000,
+  round9: 1000,
+  round10: 1000,
 }
 
 const Store = ({ children }) => {
-  const [state, dispatch] = useReducer(Reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState)
   return <Context.Provider value={[state, dispatch]}>{children}</Context.Provider>
 }
 
