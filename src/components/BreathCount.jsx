1da32c7548aa7c1262864be706f1aabfaa8cb3a0
Nathan Bell <nathanbell09@gmail.com>
2021-04-21 17:25:45 +0100

    added breath count component

---
+++ src/components/BreathCount.jsx
@@ -0,0 +1,27 @@
+import * as ACTION from '../constants/action'
+import { useGlobalState } from '../hooks/use-global-state'
+import Slider from './Slider'
+import Card from './Card'
+
+const BreathCount = () => {
+  const [state, dispatch] = useGlobalState()
+
+  const handleChange = (event, value) => {
+    dispatch(ACTION.SET_BREATH_COUNT, value)
+  }
+
+  return (
+    <Card>
+      <Slider
+        label='Breath Count'
+        type='breathCount'
+        onChange={handleChange}
+        value={state.breathCount}
+      />
+    </Card>
+  )
+}
+
+BreathCount.propTypes = {}
+
+export default BreathCount
